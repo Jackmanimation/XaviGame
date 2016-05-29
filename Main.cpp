24ef09f2097e47fe70ebd7ebeb54d08a480eb6e8
Denis Jackman <denis_jackman@hotmail.com>
2016-05-29 19:02:22 +0100

    Initial Upload of Files to kick this off

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char* args[] )
+{
+  	cout << " -- Game Version 1.0 (Test) -- " << endl;
+	cout << " --- Starting ---" << endl;
+	cout << " --- Ending ---" << endl;
+  	return 0;
+}
