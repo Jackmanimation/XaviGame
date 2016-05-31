805c17444a6a49dd0b0d32f3040dcfa70ac96b09
Denis Jackman <denis_jackman@hotmail.com>
2016-05-31 22:20:11 +0100

    Added new log file netry and debug mode

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,43 @@
+#include <fstream>
 #include <iostream>
 
 using namespace std;
+bool gDebugMode = false;
 
 int main (int argc, char* args[] )
 {
+    gDebugMode = true;
+    ofstream logfile;
+    gDebugMode = true;
+    if (gDebugMode)
+    {
+        // open a file in write mode.
+        logfile.open("/Users/denisjackman/Documents/workspace/JackmanGame/game.log");
+    }
   	cout << " -- Game Version 1.0 (Test) -- " << endl;
 	cout << " --- Starting ---" << endl;
+    if (gDebugMode)
+    {
+      	logfile << " -- Game Version 1.0 (Test) -- " << endl;
+	    logfile << " --- Starting ---" << endl;
+    }
+    int i=1;
+    for (;i<argc;i++)
+    {
+        cout << "Argument " << i << " Arg : " << args[i] << endl ;
+    }
+
+
 	cout << " --- Ending ---" << endl;
+    if (gDebugMode)
+    {
+	    logfile << " --- Ending ---" << endl;
+    }
+
+    if (gDebugMode)
+    {
+        // close the opened file.
+        logfile.close();
+    }
   	return 0;
 }
