cac70e237beb0da6c5f22857a06a001317a4bce1
Denis Jackman <denis_jackman@hotmail.com>
2016-06-26 22:51:10 +0100

    beginning the utility program

---
+++ utilities/utility.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char* args[] )
+{
+    cout << "Starting Utility Program " << endl;
+    cout << "Finishing Utility Program " << endl;
+}
